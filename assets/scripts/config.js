48ce42038e4082177264b098b9600d8808b4cb5f
pprakasam <padmaprakasam@gmail.com>
2019-03-14 14:25:28 -0400

    Change url in config.js
    change the url in config.js with heroku deployed api apiUrl

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://git.heroku.com/young-cliffs-74043.git',
+  production: 'https://young-cliffs-74043.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
